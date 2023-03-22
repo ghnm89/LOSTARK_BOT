51bbee0201bafde2a96f02712b9b8d75366320bb
unknown <herokim@root37.net>
2023-03-22 14:28:02 +0900

    Initial commit

---
+++ src/main/java/com/project/hero/HeroApplication.java
@@ -0,0 +1,13 @@
+package com.project.hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroApplication.class, args);
+    }
+
+}
